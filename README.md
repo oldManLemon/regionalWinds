3377038b82c91d6730c3b29e9b2dc23c3624576e
hase <oldm8reggie@gmail.com>
2018-08-31 11:24:17 +1000

    Project readme added

---
+++ README.md
@@ -0,0 +1,10 @@
+# Regional Winds
+
+Calculates wind pressures as per AS1170.2:2002
+
+I think the standards are under copyright which is really wierd. Thus they are currently stored in a database. 
+
+Project has being put on ICE waiting upond final approval then it will be linked up as a flask web app. In the meantime, math and table lookups are working as expected. 
+
+This math is to a higher standard than that AS1170.2:2002 require as per office policy. 
+
--- wind.py
+++ wind.py
@@ -9,26 +9,6 @@ password = 'nb'
 
 conn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
 cursor = conn.cursor()
-# cmd= "select B from dbo.RegionalWind where speed = '2000';"
-# cursor.execute(cmd)
-# rows = cursor.fetchone()
-
-# Speed = 0
-# A = 1
-# W = 2
-# B = 3
-# C = 4
-# D = 5
-# for row in rows:
-#     print(row)
-
-# #Speed Factors
-# wFive = 0
-# wTen = 1
-# wTwenty = 2
-# def factors(x,y):
-#     for row in rows:
-#         print(row)
 
 """ Data Expected """
 
